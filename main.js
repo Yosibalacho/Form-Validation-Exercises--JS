c1828e3fc09f73ab4bc8ce39411fe0e1242a238c
Yosibalacho <yosibala@gmail.com>
2022-06-11 23:07:48 +0300

    change

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function sendForm() {
   return false;
 }
 function firstAndLastName() {
-  var regexName = /^(?=.*[a-z])(?=.*[A-Z])(?=.*{1,10})$/;
+  var regexName = /^([A-Za-z])({1,10})$/;
   if (regexName.test(first_input.value && last_input.value)) {
     return true;
   }
